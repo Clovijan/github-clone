bdf5dd373675b43ad9d8da227938351f43354fd8
clovijan <clovijan@gmail.com>
2020-08-06 22:13:52 -0300

    Criação page Profile e componente ProfileData

---
+++ src/components/ProfileData/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+interface Props {
+  username: string;
+  name: string;
+  avatarUrl: string;
+  followers: number;
+  following: number;
+  company?: string;
+  location?: string;
+  email?: string;
+  blog?: string;
+}
+                                     
+const ProfileData: React.FC<Props>= ({
+  username,
+  name,
+  avatarUrl,
+  followers,
+  following,
+  company,
+  location,
+  email,
+  blog,
+}) => {
+  return <div />;
+}
+
+export default ProfileData;
--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, Main, LeftSide, RightSide, } from './styles';
+import ProfileData from '../../components/ProfileData';
+
 
 const Profile: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Main>
+        <LeftSide>
+          <ProfileData
+              username={'Clovijan'}
+              name={'Clovijan'}
+              avatarUrl={'https://avatars1.githubusercontent.com/u/37074720?v=4'}
+              followers={8}
+              following={8}
+              company={'UFS'}
+              location={'Sergipe'}
+              email={'clovijan@gmail.com'}
+              blog={'clovijan@blog'}
+            />
+        </LeftSide>
+  
+        <RightSide></RightSide>
+      </Main>
+    </Container>
+  );
 }
 
-export default Profile;
+export default Profile;   
